27f29c823145d52d01817c14d40dc5f7d733605c
SHAIK FAZUL AHAMMAD <fazulahammads@gmail.com>
2025-12-26 23:06:52 +0530

    Fix MongoDB Atlas connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/KisanCart");
-        console.log("MongoDB Connected");
-    } catch (error) {
-        console.log("MongoDB Connection Error:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1);
+  }
 };
+
 module.exports = connectDB;
