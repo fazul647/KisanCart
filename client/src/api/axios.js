391472517bb7b720bd0fd84b4c472326295a2c78
SHAIK FAZUL AHAMMAD <fazulahammads@gmail.com>
2025-12-26 23:54:33 +0530

    Fix frontend API base URL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,21 +1,21 @@
-// client/src/api/axios.js
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
-  withCredentials: false,
 });
 
-// Attach token automatically if present
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('kisan_token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => Promise.reject(error));
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("kisan_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
